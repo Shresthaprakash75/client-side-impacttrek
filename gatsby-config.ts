34161f6bfecc08fd4d7bbc3b1516e1e26411764d
prakash <light@prakashs-MacBook-Air.local>
2024-01-15 22:49:11 +0545

    real data integration using graphql

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,16 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-graphql',
+      options: {
+        typeName: 'Trek',
+        fieldName: 'trek',
+        url: 'http://localhost:4000/graphql', // Replace with your GraphQL server endpoint
+      },
+    },
+  ],
 }
 
 export default config
