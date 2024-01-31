b5f36ff25a862d060f76d02c9160e52f66698b5c
prakash <light@prakashs-MacBook-Air.local>
2024-01-31 17:41:55 +0545

    changing name : plan trek to choose trek

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <footer className="py-3 my-4">
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                     <li className="nav-item">
-                        <Link to="/choose-trek" className="nav-link px-2 text-body-secondary">Plan Trek</Link>
+                        <Link to="/treks" className="nav-link px-2 text-body-secondary">Choose Trek</Link>
                     </li>
                     <li className="nav-item">
                         <Link to="/book-trek" className="nav-link px-2 text-body-secondary">Book Trek</Link>
